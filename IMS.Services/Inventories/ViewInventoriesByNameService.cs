f0ca9f28e153efbc58885c14a35ac31ea89c0b04
kanaanayt <kanaanabukhadra@gmail.com>
2023-05-16 13:58:29 +0300

    Implement ViewInventoriesByNameService

---
+++ IMS.Services/Inventories/ViewInventoriesByNameService.cs
@@ -0,0 +1,16 @@
+﻿namespace IMS.Services.Inventories
+{
+    public class ViewInventoriesByNameService
+    {
+        private readonly IInventoryRepository inventoryRepository;
+
+        public ViewInventoriesByNameService(IInventoryRepository inventoryRepository)
+        {
+            this.inventoryRepository = inventoryRepository;
+        }
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string invName = "")
+        {
+            return await inventoryRepository.GetInventoriesByNameAsync(invName);
+        }
+    }
+}
