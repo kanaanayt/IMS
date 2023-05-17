2c2325c12f3fda5505658c722226cbd984e3c35f
kanaanayt <kanaanabukhadra@gmail.com>
2023-05-17 17:10:00 +0300

    Implement AddInventory page component, AddEditInventoryItem component

--- IMS.CoreBusiness/Inventory.cs
+++ IMS.CoreBusiness/Inventory.cs
@@ -1,10 +1,21 @@
-﻿namespace IMS.CoreBusiness
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.CoreBusiness
 {
     public class Inventory
     {
         public int InventoryId { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Invalid name")]
         public string InventoryName { get; set; } = String.Empty;
+
+        [Required]
+        [Range(1, 1000, ErrorMessage = "Invalid quantity")]
         public int Quantity { get; set; }
+
+        [Required]
+        [Range(1, 1000, ErrorMessage = "Invalid price")]
         public double Price { get; set; }
 
     }
