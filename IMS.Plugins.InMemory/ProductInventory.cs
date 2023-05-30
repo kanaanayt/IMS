07262edf26ba94eb9da041c15c68186a94b2d202
kanaanayt <kanaanabukhadra@gmail.com>
2023-05-30 11:23:18 +0300

    wip

---
+++ IMS.Plugins.InMemory/ProductInventory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Plugins.InMemory
+{
+    public class ProductInventory
+    {
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+        [ForeignKey("Inventory")]
+        public int InventoryId { get; set; }
+    }
+}
